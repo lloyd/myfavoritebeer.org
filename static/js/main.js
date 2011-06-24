87926dfda7094cbfaa00dbd29bb2164ff1e8e7c7
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-06-24 15:38:31 -0600

    enough to demo, isn't it?

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,10 @@
+function loggedIn() {
+  $("#content .intro").fadeOut(700, function() {
+    $("#content .business").fadeIn(300, function() {
+    });
+  });
+}
+
 $(document).ready(function() {
   $("#header .login").show().click(function() {
     $("#header .login").css('opacity', '0.5');
@@ -17,6 +24,13 @@ $(document).ready(function() {
               .append($("<span>").text(data.email).addClass("username"))
               .append($("<span>!</span>"));
             l.unbind('click');
+
+            var iurl = 'http://www.gravatar.com/avatar/' +
+              Crypto.MD5($.trim(data.email).toLowerCase()) +
+              "?s=32";
+            $("<img>").attr('src', iurl).appendTo($("#header .picture"));
+
+            loggedIn();
           },
           error: function(jqXHR, textStatus, errorThrown) {
             $("#header .login").css('opacity', '1');
