da76adf5057f5c1d88e2d33b8b494001b6f71962
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-06-24 14:14:29 -0600

    basic trivial first cut

---
+++ static/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $("#header .login").show().click(function() {
+    $("#header .login").css('opacity', '0.5');
+    navigator.id.getVerifiedEmail(function(assertion) {
+      if (assertion) {
+        // Now we'll send this assertion over to the verification server for validation
+        var url = "https://browserid.org/verify?assertion=" + window.encodeURIComponent(assertion) +
+          "&audience=" + window.encodeURIComponent(window.location.host);
+        $.ajax({
+          url: url,
+          dataType: "json",
+          success: function(data, textStatus, jqXHR) {
+            var l = $("#header .login").removeClass('clickable');;
+            l.empty();
+            l.css('opacity', '1');
+            l.append($("<span>").text("Yo, "))
+              .append($("<span>").text(data.email).addClass("username"))
+              .append($("<span>!</span>"));
+            l.unbind('click');
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+            $("#header .login").css('opacity', '1');
+          }
+        });
+      } else {
+        // something went wrong!  the user isn't logged in.
+        $("#header .login").css('opacity', '1');
+      }
+    });
+  }).addClass("clickable");
+});
