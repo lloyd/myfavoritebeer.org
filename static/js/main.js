1c80b3858068e89f68f3a5a74afc8badeb4bac71
Shane Tomlinson <stomlinson@mozilla.com>
2011-09-23 06:37:59 +0100

    When adding the sign in button, set the opacity to 1.0.
    This is because if the button was already there, it is not replaced and redrawn but reused with it's former opacity of 0.5
    
    issue #20.

--- static/js/main.js
+++ static/js/main.js
@@ -73,7 +73,7 @@ function loggedOut() {
     .show().click(function() {
       $("header .login").css('opacity', '0.5');
       navigator.id.getVerifiedEmail(gotVerifiedEmail);
-    }).addClass("clickable");
+    }).addClass("clickable").css('opacity','1.0');
 }
 
 function gotVerifiedEmail(assertion) {
